5f6ac4d4391462dfbcf3a155f29aa7109a91f5c6
Chorizoiberico17 <alu.129156@usj.es>
2024-04-03 16:57:57 +0200

    Added methods getValue & setValue, and initialiced the constructor Sent headers

--- ProjectCode/SentHeaders.java
+++ ProjectCode/SentHeaders.java
@@ -7,10 +7,38 @@ public class SentHeaders {
     List<String> headers;
     public SentHeaders() {
         this.headers = new ArrayList<>();
+        for (DictionaryHeaders headerType : DictionaryHeaders.values()) {
+            addHeader(headerType, "");
+        }
     }
 
     public void addHeader(Enum headerType, String headerValue) {
         String header = headerType.toString() + ": " + headerValue;
         this.headers.add(header);
+    }
+
+    public String getValue(Enum headerType) {
+        for (String header : this.headers) {
+            if (header.contains(headerType.toString())) {
+                return header.split(": ")[1];
+            }
+        }
+        return null;
+    }
+    
+    public void setValue(Enum headerType, String headerValue) {
+        for (int i = 0; i < this.headers.size(); i++) {
+            if (this.headers.get(i).contains(headerType.toString())) {
+                this.headers.set(i, headerType.toString() + ": " + headerValue);
+            }
+        }
+    }
+
+    public String toString() {
+        String headers = "";
+        for (String header : this.headers) {
+            headers += header + "\r\n";
+        }
+        return headers;
     }
 }
