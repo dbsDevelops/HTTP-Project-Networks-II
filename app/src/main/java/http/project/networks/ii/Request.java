9471810dcb6fe207384436cbc0aec7f52d60c416
dbsDevelops <alu.129152@usj.es>
2024-04-06 17:08:40 +0200

    Swapped HEAD and DELETE in the Request constructor

--- app/src/main/java/http/project/networks/ii/Request.java
+++ app/src/main/java/http/project/networks/ii/Request.java
@@ -14,9 +14,9 @@ public class Request {
         this.url = url;
         this.protocolVersion = protocolVersion;
         this.headers = new SentHeaders(method, url);
-        if(this.method == Verbs.GET || this.method == Verbs.DELETE) {
+        if(this.method == Verbs.GET || this.method == Verbs.HEAD) {
             this.body = new HttpRequestBody(HttpBodyType.RAW, "");
-        } else { //POST, PUT, HEAD
+        } else { //POST, PUT, DELETE
             this.body = new HttpRequestBody(bodyType, bodyContent);
             //Set the new values for the headers content-length and content-type
             this.headers.setValue(HttpHeaders.CONTENT_LENGTH, Integer.toString(this.body.getContentLength()));
