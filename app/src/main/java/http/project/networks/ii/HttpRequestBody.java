a9dbd978230e099631a68230a24439e88a6f1d5a
dbsDevelops <alu.129152@usj.es>
2024-04-10 17:02:26 +0200

    Removed not needed methods of HttpRequestBody

--- app/src/main/java/http/project/networks/ii/HttpRequestBody.java
+++ app/src/main/java/http/project/networks/ii/HttpRequestBody.java
@@ -7,43 +7,6 @@ public class HttpRequestBody {
     public HttpRequestBody(HttpBodyType type, String content) {
         this.type = type;
         this.content = content;
-        if (type == HttpBodyType.RAW) {
-            buildRawBody(this.content);
-        } else if (type == HttpBodyType.FORM) {
-            buildFormBody(this.content);
-        } else if (type == HttpBodyType.JSON) {
-            buildJsonBody(this.content);
-        } else if (type == HttpBodyType.FILE) {
-            buildFileBody(this.content);
-        } else if (type == HttpBodyType.GRAPHQL) {
-            buildGraphQLBody(this.content);
-        } else if (type == HttpBodyType.XML) {
-            buildXmlBody(this.content);
-        }
-    }
-
-    public void buildRawBody(String content) {
-        System.out.println("Raw body: " + content);
-    }
-
-    public void buildFormBody(String content) {
-        System.out.println("Form body: " + content);
-    }
-
-    public void buildJsonBody(String content) {
-        System.out.println("JSON body: " + content);
-    }
-
-    public void buildFileBody(String content) {
-        System.out.println("File body: " + content);
-    }
-
-    public void buildGraphQLBody(String content) {
-        System.out.println("GraphQL body: " + content);
-    }
-
-    public void buildXmlBody(String content) {
-        System.out.println("XML body: " + content);
     }
 
     public void setType(HttpBodyType type) {
