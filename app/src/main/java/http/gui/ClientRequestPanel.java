5dc145ebb4ee9c32fcb191afe5c84c6c353a8f49
dbsDevelops <alu.129152@usj.es>
2024-04-18 12:45:57 +0200

    Modified combo box to a new dialog with checkboxes

--- app/src/main/java/http/gui/ClientRequestPanel.java
+++ app/src/main/java/http/gui/ClientRequestPanel.java
@@ -1,22 +1,32 @@
 package http.gui;
 
+import java.util.ArrayList;
+
 import javax.swing.JPanel;
 import javax.swing.JLabel;
 import javax.swing.JTextField;
+import http.project.networks.ii.HttpHeaders;
+
 import javax.swing.BorderFactory;
 import javax.swing.JSplitPane;
-import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JScrollPane;
+import javax.swing.JDialog;
 
 import java.awt.GridLayout;
+import java.awt.Checkbox;
+import java.awt.event.ActionListener;
 import java.awt.BorderLayout;
 import java.awt.Container;
 import java.awt.Dimension;
+import java.awt.CheckboxGroup;
 
 public class ClientRequestPanel extends JPanel {
 
     protected static final String HOST_STRING = "Host ";
     protected static final String PORT_STRING = "Port ";
     protected static final String HEADERS_STRING = "Headers";
+    protected static final int ROWS_AND_COLUMNS = 4;
     
     // Lay out the text controls and labels
     JLabel hostLabel = new JLabel(HOST_STRING);
@@ -24,12 +34,15 @@ public class ClientRequestPanel extends JPanel {
     JLabel headersLabel = new JLabel(HEADERS_STRING);
     JTextField hostField = new JTextField(40);
     JTextField portField = new JTextField(3);
-    JComboBox<String> headersComboBox = new JComboBox<>();
+    ArrayList<Checkbox> headerCheckboxes = new ArrayList<>();
+    JButton addHeadersButton = new JButton("Add Headers");
+    JDialog addHeadersDialog = new JDialog();
+    JButton sendButton = new JButton("Send");
 
     JPanel requestControlsPanel = new JPanel();
-    GridLayout gridLayout = new GridLayout(2,2);
+    GridLayout gridLayout = new GridLayout(ROWS_AND_COLUMNS, ROWS_AND_COLUMNS);
 
-    JPanel responseControlsPanel = new JPanel();
+    JScrollPane responseControlsPanel = new JScrollPane();
 
     public ClientRequestPanel() {
         setLayout(new BorderLayout());
@@ -40,7 +53,19 @@ public class ClientRequestPanel extends JPanel {
         JTextField[] textFields = {hostField, portField};
         requestControlsPanel.setLayout(gridLayout);
         addLabelTextRows(labels, textFields, requestControlsPanel);
-        requestControlsPanel.setPreferredSize(new Dimension(500, 300));
+
+        addHeadersButton.addActionListener(new ActionListener() {
+           public void actionPerformed(java.awt.event.ActionEvent e) {
+               addHeaderCheckboxes();
+               addHeadersDialog.setTitle("Extra Headers");
+               
+               addHeadersDialog.setVisible(true);
+           }
+        });
+
+        requestControlsPanel.add(addHeadersButton);
+        requestControlsPanel.add(sendButton);
+        //requestControlsPanel.setPreferredSize(new Dimension(500, 300));
         requestControlsPanel.setMinimumSize(new Dimension(300, 100));
         requestControlsPanel.setBorder(
             BorderFactory.createCompoundBorder(
@@ -48,12 +73,12 @@ public class ClientRequestPanel extends JPanel {
                 BorderFactory.createEmptyBorder(5,5,5,5)
             )
         );
-        add(requestControlsPanel);
 
         // Set up right panel (resoponses)
-        responseControlsPanel.setLayout(new BorderLayout());
-        requestControlsPanel.setPreferredSize(new Dimension(500, 300));
-        requestControlsPanel.setMinimumSize(new Dimension(300, 100));
+        //responseControlsPanel.setLayout(new BorderLayout());
+        //requestControlsPanel.setPreferredSize(new Dimension(500, 300));
+        responseControlsPanel.setMinimumSize(new Dimension(300, 100));
+        responseControlsPanel.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
         responseControlsPanel.setBorder(
             BorderFactory.createCompoundBorder(
                 BorderFactory.createTitledBorder("Response"),
@@ -63,16 +88,15 @@ public class ClientRequestPanel extends JPanel {
         
         // Set up split panel
         JSplitPane splitPanel = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT, requestControlsPanel, responseControlsPanel);
+        splitPanel.setOneTouchExpandable(true);
+        splitPanel.setResizeWeight(0.5);
         add(splitPanel);
-
-        
     }
 
     private void addLabelTextRows(JLabel[] labels, JTextField[] textFields, Container container) {
         // c = new GridBagConstraints();
         // c.anchor = GridBagConstraints.EAST;
         int numLabels = labels.length;
-
         addLabelsToContainer(numLabels, labels, textFields, container);
     }
 
@@ -84,6 +108,13 @@ public class ClientRequestPanel extends JPanel {
             labels[currentLabelIndex].setLabelFor(textFields[currentLabelIndex]);
             container.add(labels[currentLabelIndex]);
             container.add(textFields[currentLabelIndex]);
+        }
+    }
+
+    public void addHeaderCheckboxes() {
+        for (String header: HttpHeaders.getHeaders()) {
+            Checkbox headerCheckbox = new Checkbox(header);
+            headerCheckboxes.add(headerCheckbox);
         }
     }
 }
