9febd661ff9e71de2e00504b4e68ed77fc218dba
dbsDevelops <alu.129152@usj.es>
2024-05-02 16:44:44 +0200

    Solved issue in Windows for log file

--- app/src/main/java/http/project/networks/ii/logger/Logger.java
+++ app/src/main/java/http/project/networks/ii/logger/Logger.java
@@ -6,7 +6,10 @@ import java.io.IOException;
 import java.io.OutputStreamWriter;
 import java.nio.charset.StandardCharsets;
 
+import java.text.SimpleDateFormat;
+
 import java.time.LocalDateTime;
+import java.util.Date;
 
 public class Logger {
 
@@ -14,45 +17,52 @@ public class Logger {
     public static final int WARNING = 1;
     public static final int ERROR = 2;
 
+    private static final String DATE_FORMAT = "yyyy-MM-dd_HH:mm:ss";
     private static final String USER_DIR_PROPERTY = "user.dir";
     private static final String DIRECTORY_PATH = "/app/src/main/java/http/project/networks/ii/logger/logs/";
     private static final String LOG_FILE_EXTENSION = ".log";
 
-    private static String ANSI_RESET = "\u001B[0m";
-    private static String ANSI_RED = "\u001B[31m";
-    private static String ANSI_YELLOW = "\u001B[33m";
+    private static final String ANSI_RESET = "\u001B[0m";
+    private static final String ANSI_RED = "\u001B[31m";
+    private static final String ANSI_YELLOW = "\u001B[33m";
 
     private String logPath;
+    private SimpleDateFormat simpleDateFormat;
+    String dateText;
     private FileOutputStream fileOutputStream;
     private OutputStreamWriter outputStreamWriter;
 
 
     public Logger(String logPath) {
         this.logPath = logPath;
+        simpleDateFormat = new SimpleDateFormat(DATE_FORMAT);
+        dateText = simpleDateFormat.format(new Date());
         try {
-            this.fileOutputStream = new FileOutputStream(System.getProperty(USER_DIR_PROPERTY) + DIRECTORY_PATH + logPath + LocalDateTime.now().toString() + LOG_FILE_EXTENSION, true);
+            this.fileOutputStream = new FileOutputStream(System.getProperty(USER_DIR_PROPERTY) + DIRECTORY_PATH + logPath + dateText + LOG_FILE_EXTENSION, true);
             this.outputStreamWriter = new OutputStreamWriter(fileOutputStream, StandardCharsets.UTF_8);
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
+            log(e.getMessage(), ERROR);
         }
     }
 
     public void log(String message, int type) {
         try {
             if(type == ERROR) {
-                outputStreamWriter.write(ANSI_RED + LocalDateTime.now().toString() + " - " + message + "\n" + ANSI_RESET);
+                outputStreamWriter.write(ANSI_RED + LocalDateTime.now().toString() + "-" + message + "\n" + ANSI_RESET);
                 outputStreamWriter.flush();
             } else if (type == WARNING) {
-                outputStreamWriter.write(ANSI_YELLOW + LocalDateTime.now().toString() + " - " + message + "\n"+ ANSI_RESET);
+                outputStreamWriter.write(ANSI_YELLOW + LocalDateTime.now().toString() + "-" + message + "\n"+ ANSI_RESET);
                 outputStreamWriter.flush();
             } else {
-                outputStreamWriter.write(LocalDateTime.now().toString() + " - " + message + "\n");
+                outputStreamWriter.write(LocalDateTime.now().toString() + "-" + message + "\n");
                 outputStreamWriter.flush();
             }
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
+            log(e.getMessage(), ERROR);
         }
     }
 
@@ -63,6 +73,7 @@ public class Logger {
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
+            log(e.getMessage(), ERROR);
         }
     }
 
