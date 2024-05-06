edfe261eebdfa20ddd0ad3746d6ffad6112fd6d6
Chorizoiberico17 <alu.129156@usj.es>
2024-05-06 16:14:45 +0200

    Added a ClientCLI to simulate a curl. First part

---
+++ app/src/main/java/http/project/networks/ii/client/ClientCLI.java
@@ -0,0 +1,14 @@
+package http.project.networks.ii.client;
+
+import joptsimple.OptionParser;
+import joptsimple.OptionSet;
+
+public class ClientCLI {
+    public static void main(String[] args) {
+        OptionParser parser = new OptionParser(){
+            //Content to parse
+        };
+        OptionSet options = parser.parse(args);
+        
+    }
+}
