b288c03d237c2f9cfcdfb1ad382e0e4fdf2b8648
ivanbisimbrero <alu.135046@usj.es>
2024-05-08 20:04:09 +0200

    Created the ClientCLI main class

--- app/src/main/java/http/project/networks/ii/client/ClientCLI.java
+++ app/src/main/java/http/project/networks/ii/client/ClientCLI.java
@@ -1,14 +1,70 @@
 package http.project.networks.ii.client;
 
+import http.project.networks.ii.headers.RequestHeaders;
+import http.project.networks.ii.requests.Request;
+import http.project.networks.ii.utils.HTTPUtils;
+import http.project.networks.ii.utils.HttpBodyType;
+import http.project.networks.ii.utils.Verbs;
 import joptsimple.OptionParser;
 import joptsimple.OptionSet;
+import joptsimple.OptionSpec;
+
+import java.net.URL;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.util.List;
 
 public class ClientCLI {
     public static void main(String[] args) {
-        OptionParser parser = new OptionParser(){
-            //Content to parse
-        };
+        OptionParser parser = new OptionParser();
+        parser.acceptsAll(List.of("X", "request"), "HTTP method type").withRequiredArg().defaultsTo("GET");
+        parser.acceptsAll(List.of("H", "header"), "HTTP headers").withRequiredArg();
+        parser.acceptsAll(List.of("d", "data"), "Data to be sent as the request body").withRequiredArg();
+        parser.acceptsAll(List.of("h", "help"), "Display help").forHelp();
+
+        OptionSpec<String> urlSpec = parser.nonOptions("URL to request").ofType(String.class).describedAs("url");
+
         OptionSet options = parser.parse(args);
-        
+
+        String method = (String) options.valueOf("request");
+        String data = (String) options.valueOf("data");
+        List<?> headers = options.valuesOf("header");
+        String urlString = options.valueOf(urlSpec);
+
+        if (options.has("help") || urlString == null) {
+            printHelp(parser);
+            return;
+        }
+
+        try {
+            URL url = new URL(urlString);
+            GreetClient client = new GreetClient(HTTPUtils.HTTP_PORT);
+            RequestHeaders requestHeaders = new RequestHeaders(url);
+            for (Object header : headers) {
+                String[] splitHeader = ((String) header).split(":");
+                if (splitHeader.length == 2) {
+                    requestHeaders.addHeaderToHeaders(splitHeader[0].trim(), splitHeader[1].trim());
+                }
+            }
+
+            Verbs verb = Verbs.valueOf(method.toUpperCase());
+            HttpBodyType bodyType = HttpBodyType.JSON;  // Default, adjust based on actual data type
+            Request request = new Request(verb, url, "HTTP/1.1", requestHeaders, bodyType, data != null ? data : "");
+            
+            client.sendRequest(url, request);
+        } catch (MalformedURLException e) {
+            System.err.println("Invalid URL: " + urlString);
+        } catch (IllegalArgumentException e) {
+            System.err.println("Error: " + e.getMessage());
+        }
+    }
+
+    private static void printHelp(OptionParser parser) {
+        try {
+            System.out.println("Usage: java -jar app/build/libs/app.jar [url] (params...)");
+            parser.printHelpOn(System.out);
+        } catch (IOException e) {
+            System.err.println("Could not print help: " + e.getMessage());
+        }
     }
 }
