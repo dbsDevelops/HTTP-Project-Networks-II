b1a1ea32abf638108f3e5e86a1dae8d583b6d8fe
ivanbisimbrero <alu.135046@usj.es>
2024-05-11 19:29:07 +0200

    Created Docker deployment for the server.

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM openjdk:11-jdk
+
+RUN apt-get update && \
+    apt-get install -y gradle git && \
+    rm -rf /var/lib/apt/lists/*
+
+RUN git clone "https://github.com/dbsDevelops/HTTP-Project-Networks-II.git"
+
+WORKDIR /HTTP-Project-Networks-II
+
+RUN chmod +x gradlew && \
+    ./gradlew clean build
+
+EXPOSE 80
+
+CMD ["java", "-jar", "/HTTP-Project-Networks-II/app/build/libs/ServerDocker.jar"]
---
+++ app/src/main/java/http/project/networks/ii/server/ServerDockerApp.java
@@ -0,0 +1,17 @@
+package http.project.networks.ii.server;
+
+import http.project.networks.ii.utils.HTTPUtils;
+
+public class ServerDockerApp {
+
+    public static void main(String[] args) {
+        
+        String staticFilesDir = System.getenv("STATIC_FILES_DIR");
+        if (staticFilesDir == null) {
+            staticFilesDir = "/HTTP-Project-Networks-II/app/src/main/java/http/project/networks/ii/static_resources";
+        }
+        GreetServer gs = new GreetServer(staticFilesDir);
+        gs.initServer(HTTPUtils.HTTP_PORT);
+    }
+
+}
