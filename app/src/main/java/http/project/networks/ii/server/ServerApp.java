bfa2d70179f727ec9fbe0aa3397c2a5fd9d32803
Ger1y1 <63232421+Ger1y1@users.noreply.github.com>
2024-05-20 10:08:08 +0200

    Support for HTTP and HTTPS at the same time

--- app/src/main/java/http/project/networks/ii/server/ServerApp.java
+++ app/src/main/java/http/project/networks/ii/server/ServerApp.java
@@ -5,8 +5,32 @@ import http.project.networks.ii.utils.HTTPUtils;
 public class ServerApp {
 
     public static void main(String[] args) {
-        GreetServer gs = new GreetServer("app/src/main/java/http/project/networks/ii/static_resources", HTTPUtils.HTTPS_PORT);
-        gs.initServer();
-    }
+        // Create a runnable for the HTTPS server
+        Runnable httpsServerTask = () -> {
+            GreetServer gshttps = new GreetServer("app/src/main/java/http/project/networks/ii/static_resources", HTTPUtils.HTTPS_PORT);
+            gshttps.initServer();
+        };
 
+        // Create a runnable for the HTTP server
+        Runnable httpServerTask = () -> {
+            GreetServer gshttp = new GreetServer("app/src/main/java/http/project/networks/ii/static_resources", HTTPUtils.HTTP_PORT);
+            gshttp.initServer();
+        };
+
+        // Create threads for each server task
+        Thread httpsThread = new Thread(httpsServerTask);
+        Thread httpThread = new Thread(httpServerTask);
+
+        // Start the threads
+        httpsThread.start();
+        httpThread.start();
+
+        // Optionally, join the threads if you need the main thread to wait for them to finish
+        try {
+            httpsThread.join();
+            httpThread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
